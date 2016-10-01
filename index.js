8690fb1158a4e288e442a18d6230680f63b5acdc
BuildTools <unconfigured@null.spigotmc.org>
2016-10-01 21:05:10 +0100

    testing

--- index.js
+++ index.js
@@ -407,13 +407,8 @@ controller.hears(["\b\b"], ['ambient', 'direct_message', 'direct_mention', 'ment
   bot.reply(message, multi_res(responses))
 })
 */
-controller.hears(["[\bTEST1\b]"], ['ambient', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
+controller.hears(["\b[TEST]\b"], ['ambient', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
   var responses = ["Success1"];
-    
-  bot.reply(message, multi_res(responses))
-})
-controller.hears(["[\b]TEST2[\b]"], ['ambient', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
-  var responses = ["Success2"];
     
   bot.reply(message, multi_res(responses))
 })
